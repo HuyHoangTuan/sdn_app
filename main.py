f60d0d6a5952bd463d8c37ec751af2cf03e8db40
Hoàng Tuấn Huy <kubin2712@gmail.com>
2023-06-03 00:10:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from mininet.cli import CLI
-from src.mininet_core import mininet_core, topo_core
-from mininet.node import RemoteController
+# from mininet.cli import CLI
+# from src.mininet_core import mininet_core, topo_core
+# from mininet.node import RemoteController
 
 # if __name__ == '__main__':
     # net = mininet_core.create_mininet(topo_core.Topology())
