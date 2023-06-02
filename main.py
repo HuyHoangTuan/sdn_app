2abff3e5444ac406a80178e5525aec22d2ff81e2
HuyHt <kubin2712@gmail.com>
2023-06-02 18:00:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from mininet.cli import CLI
 from src.mininet_core import mininet_core, topo_core
+from mininet.node import RemoteController
 
 if __name__ == '__main__':
     net = mininet_core.create_mininet(topo_core.Topology())
+    net.addController("c0", controller= RemoteController, ip= "127.0.0.1", port= 6653)
     net.start()
     CLI(net)
     net.stop()
